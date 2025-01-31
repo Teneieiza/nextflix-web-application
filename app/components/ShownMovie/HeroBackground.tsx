1da5bdd33608740aebfdccc9ba6eac337b9f3f58
tenmonster <tenmonster@hotmail.com>
2025-01-31 11:23:34 +0700

    Create HeroBackground

---
+++ app/components/ShownMovie/HeroBackground.tsx
@@ -0,0 +1,17 @@
+import Image from 'next/image'
+
+export default function HeroBackground() {
+  return (
+    <div>
+      <Image
+        src="/main/mainshown.svg"
+        alt="mainshownimg"
+        fill
+        className="object-cover object-[70%] lg:object-contain"
+        priority
+      />
+      <div className="absolute inset-0 from-black/60 to-transparent md:bg-gradient-to-r" />
+      <div className="absolute bottom-0 h-[50%] w-full bg-gradient-to-t from-black/100 to-transparent md:bg-gradient-to-t" />
+    </div>
+  )
+}
