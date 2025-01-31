73ed899d5689a2ef285ab8642cdae062947432a8
tenmonster <tenmonster@hotmail.com>
2025-01-31 21:25:46 +0700

    Add MovieSnapSection to HomePage

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,19 @@
-import HeroInfo from './components/ShownMovie/HeroInfo'
-import HeroBackground from './components/ShownMovie/HeroBackground'
+import HeroInfo from './components/home/hero/HeroInfo'
+import HeroBackground from './components/home/hero/HeroBackground'
+import MovieSnap from './components/home/movie/MovieSnap'
 
 export default function HomePage() {
   return (
     <div className="flex h-full w-full flex-col items-center">
-      <section className="absolute h-[65vh] w-full lg:h-[56vw]">
+      {/* Hero Section */}
+      <section className="relative h-[70vh] w-full">
         <HeroBackground />
         <HeroInfo />
+      </section>
+
+      {/* MovieSnap Section */}
+      <section className="w-full relative">
+        <MovieSnap />
       </section>
     </div>
   )
